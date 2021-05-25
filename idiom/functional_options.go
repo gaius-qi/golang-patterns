d722cbe24234cc726198b3936b4e3e69cd8164e1
Gaius <gaius.qi@gmail.com>
2021-05-25 20:55:54 +0800

    feat: functional options
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ idiom/functional_options.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"os"
+)
+
+type Options struct {
+	UID         int
+	GID         int
+	Flags       int
+	Contents    string
+	Permissions os.FileMode
+}
+
+type Option func(*Options)
+
+func UID(userID int) Option {
+	return func(args *Options) {
+		args.UID = userID
+	}
+}
+
+func GID(groupID int) Option {
+	return func(args *Options) {
+		args.GID = groupID
+	}
+}
+
+func Contents(c string) Option {
+	return func(args *Options) {
+		args.Contents = c
+	}
+}
+
+func Permissions(perms os.FileMode) Option {
+	return func(args *Options) {
+		args.Permissions = perms
+	}
+}
+
+func New(filepath string, setters ...Option) error {
+	// Default Options
+	args := &Options{
+		UID:         os.Getuid(),
+		GID:         os.Getgid(),
+		Contents:    "",
+		Permissions: 0666,
+		Flags:       os.O_CREATE | os.O_EXCL | os.O_WRONLY,
+	}
+
+	for _, setter := range setters {
+		setter(args)
+	}
+
+	f, err := os.OpenFile(filepath, args.Flags, args.Permissions)
+	if err != nil {
+		return err
+	} else {
+		defer f.Close()
+	}
+
+	if _, err := f.WriteString(args.Contents); err != nil {
+		return err
+	}
+
+	return f.Chown(args.UID, args.GID)
+}
+
+func main() {
+	f1 := "./emptyFile.txt"
+	if err := New(f1); err != nil {
+		panic(err)
+	}
+	defer os.Remove(f1)
+
+	f2 := "./file.txt"
+	if err := New(f2, Contents("Lorem Ipsum Dolor Amet")); err != nil {
+		panic(err)
+	}
+	defer os.Remove(f2)
+}
